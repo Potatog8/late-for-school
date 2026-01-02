53442f80bda715b31b9954cedb5053309c81036d
Potato <hs.gar08@gmail.com>
2026-01-02 12:44:35 -0800

    Added gravity and jumping
    Player can now jump but there are no restrictions to how many time they can jump in the air yet since the ground has no collision still. I also added gravity so the player is constantly falling unless they jump. One problem I think I'll have is since my code updates gravity every frame to give the effect it's getting stronger, if a player falls into a pit then the gravity will just instantly suck them to the bottom. I can probably prevent this by capping gravity.

--- player.gd
+++ player.gd
@@ -2,55 +2,75 @@ extends CharacterBody2D
 
 @export var speed = 85
 @export var speedMultiplier = 2.0
+@export var gravity = 10
+@export var jumpspeed = 400
 
-var last_direction := Vector2(1,0)
+var last_direction = 1
 
 func _physics_process(_delta: float) -> void:
-	var direction
-	if Input.is_action_pressed("left"):
-		direction = Vector2(-1, 0)
-	elif Input.is_action_pressed("right"):
-		direction = Vector2(1, 0)
+	
+	var direction : Vector2
+	
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jumpspeed
 	else:
-		direction = Vector2(0,0)
+		direction.y = gravity
+	
+	if Input.is_action_pressed("left"):
+		direction.x = -1
+	elif Input.is_action_pressed("right"):
+		direction.x = 1
+	else:
+		direction.x = 0
 		
 	if Input.is_action_pressed("run"):
-		velocity = direction * (speed * speedMultiplier)
+		velocity.x = direction.x * (speed * speedMultiplier)
 	else:
-		velocity = direction * speed
+		velocity.x = direction.x * speed
+		
+	velocity.y += gravity
 	
 	move_and_slide()
-	
-	if Input.is_action_pressed("run") and direction.length() > 0:
-		last_direction = direction
-		run_anim(direction)
-	elif direction.length() > 0:
-		last_direction = direction
-		walk_anim(direction)
+	if Input.is_action_pressed("jump"):
+		jump_anim(last_direction)
+		
+	elif Input.is_action_pressed("run") and direction.length() > 0:
+		last_direction = direction.x
+		run_anim(direction.x)
+	elif abs(direction.x) > 0:
+		last_direction = direction.x
+		walk_anim(direction.x)
 	else:
 		idle_anim(last_direction)
 	
-	
+func jump_anim(direction):
+	if direction < 0:
+		$PlayerAnimation.flip_h = true
+		$PlayerAnimation.play("jump")
+	if direction > 0:
+		$PlayerAnimation.flip_h = false
+		$PlayerAnimation.play("jump") 
+		
 func run_anim(direction):
-	if direction.x < 0:
+	if direction < 0:
 		$PlayerAnimation.flip_h = true
 		$PlayerAnimation.play("run")
-	if direction.x > 0:
+	if direction > 0:
 		$PlayerAnimation.flip_h = false
 		$PlayerAnimation.play("run") 
 	
 func walk_anim(direction):
-	if direction.x < 0:
+	if direction < 0:
 		$PlayerAnimation.flip_h = true
 		$PlayerAnimation.play("walk")
-	if direction.x > 0:
+	if direction > 0:
 		$PlayerAnimation.flip_h = false
 		$PlayerAnimation.play("walk")
 
 func idle_anim(direction):
-	if direction.x < 0:
+	if direction < 0:
 		$PlayerAnimation.flip_h = true
 		$PlayerAnimation.play("idle")
-	if direction.x > 0:
+	if direction > 0:
 		$PlayerAnimation.flip_h = false
 		$PlayerAnimation.play("idle")
