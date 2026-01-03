3962ab94b1fe6ef62cfefcb0de7aec2e9ab2ef98
Potato <hs.gar08@gmail.com>
2026-01-02 23:42:43 -0800

    Added wallslide and coyote time
    Added wallslide when against a wall and in the air and pressing against the wall. Also added coyote time to make jumping smoother

--- player.gd
+++ player.gd
@@ -3,19 +3,18 @@ extends CharacterBody2D
 @export var speed = 85
 @export var speedMultiplier = 2.0
 @export var gravity = 10
-@export var jumpspeed = 400
+@export var jumpspeed = 250
+
+var wallslide = 4
+
 
 var last_direction = 1
 
 func _physics_process(_delta: float) -> void:
 	
+	var was_on_floor
 	var direction : Vector2
-	
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = -jumpspeed
-	else:
-		direction.y = gravity
-	
+
 	if Input.is_action_pressed("left"):
 		direction.x = -1
 	elif Input.is_action_pressed("right"):
@@ -28,12 +27,26 @@ func _physics_process(_delta: float) -> void:
 	else:
 		velocity.x = direction.x * speed
 		
-	velocity.y += gravity
+	if Input.is_action_pressed("jump") and (is_on_floor() or !$CoyoteTimer.is_stopped()):
+		velocity.y = -jumpspeed
+		
+	if not is_on_floor() and not is_on_wall():
+		velocity.y += gravity
+		
+	if is_on_wall() and direction.x > 0 and Input.is_action_pressed("right") and velocity.y > 0:
+		velocity.y = gravity * wallslide
+	if is_on_wall() and direction.x < 0 and Input.is_action_pressed("left") and velocity.y > 0:
+		velocity.y = gravity * wallslide
+	
+	was_on_floor = is_on_floor()
 	
 	move_and_slide()
-	if Input.is_action_pressed("jump"):
-		jump_anim(last_direction)
-		
+	
+	if was_on_floor && !is_on_floor():
+		$CoyoteTimer.start()
+	
+	if Input.is_action_pressed("jump") or velocity.y > 0:
+		jump_anim(direction.x)
 	elif Input.is_action_pressed("run") and direction.length() > 0:
 		last_direction = direction.x
 		run_anim(direction.x)
@@ -42,7 +55,10 @@ func _physics_process(_delta: float) -> void:
 		walk_anim(direction.x)
 	else:
 		idle_anim(last_direction)
-	
+
+
+
+
 func jump_anim(direction):
 	if direction < 0:
 		$PlayerAnimation.flip_h = true
