3dde4faaada77df6a1b33ddcdd65523a05303d21
Potato <hs.gar08@gmail.com>
2026-01-05 23:06:16 -0800

    Finished wall jump functionality and tweaked walking
    Wall jumping and sliding is now fully functional. It doesn't have animations for sliding on the wall yet though. Also fixed a bug with walking and tweaked the non run walk to work similarly to the running. Fixed a bug where the player would get stuck on a wall if the held sprint. Changed it so that you don't slide on the wall if you are holding jump allowing you a way to free fall while in contact with a wall. Also added some commenting

--- player.gd
+++ player.gd
@@ -1,14 +1,14 @@
-extends CharacterBody2D
+extends CharacterBody2D #Parent Node
 
-@export var speed = 85
-@export var speedMultiplier = 2.0
-@export var gravity = 10
-@export var jumpspeed = 250
+@export var speed = 160 # used for speed calculations
+@export var speedMultiplier = 30 #how much the character velocity will increase / decrease by
+@export var gravity = 10 #used to pull player down
+@export var jumpspeed = 50 #used to calculate players upward momentum during jump
 
-var wallslide = 4
+var wallslide = 4 #used to calculate wallslide speed
 
 
-var last_direction = 1
+var last_direction = 1 #stores the direction the character most recently faced
 
 func _physics_process(_delta: float) -> void:
 	
@@ -22,30 +22,98 @@ func _physics_process(_delta: float) -> void:
 	else:
 		direction.x = 0
 		
-	if Input.is_action_pressed("run"):
-		velocity.x = direction.x * (speed * speedMultiplier)
-	else:
-		velocity.x = direction.x * speed
+	if Input.is_action_pressed("run") and (!is_on_wall() or is_on_floor()):
+		if direction.x > 0:
+			if $RunTimer.is_stopped():
+				if velocity.x < 1:
+					velocity.x += speed
+				if velocity.x > 0:
+					velocity.x += speedMultiplier
+				$RunTimer.start()
+				if velocity.x > 210:
+					velocity.x = 210
+
+		if direction.x < 0:
+			if $RunTimer.is_stopped():
+				if velocity.x > -1:
+					velocity.x -= speed
+				if velocity.x < 0:
+					velocity.x -= speedMultiplier
+				$RunTimer.start()
+				if velocity.x < -210:
+					velocity.x = -210
 		
-	if Input.is_action_pressed("jump") and (is_on_floor() or !$CoyoteTimer.is_stopped()):
-		velocity.y = -jumpspeed
+		if direction.x == 0:
+			if last_direction > 0:
+				velocity.x -= speed * 0.1
+				if velocity.x < 0:
+					velocity.x = 0
+			if last_direction < 0:
+				velocity.x += speed * 0.1
+				if velocity.x > 0:
+					velocity.x = 0
+
+	if !Input.is_action_pressed("run") and (!is_on_wall() or is_on_floor()) and $WalljumpTimer.is_stopped():
+		if direction.x > 0:
+			if $RunTimer.is_stopped():
+				if velocity.x < 1:
+					velocity.x += speed * 0.5
+				if velocity.x > 0:
+					velocity.x += speedMultiplier
+				$RunTimer.start()
+				if velocity.x > 85:
+					velocity.x = 85
+					
+		if direction.x < 0:
+			if $RunTimer.is_stopped():
+				if velocity.x > -1:
+					velocity.x -= speed * 0.5
+				if velocity.x < 0:
+					velocity.x -= speedMultiplier
+				$RunTimer.start()
+				if velocity.x < -85:
+					velocity.x = -85
+					
+		if direction.x == 0:
+			velocity.x = 0
+
+
+	if Input.is_action_pressed("jump") and (is_on_floor_only() or !$CoyoteTimer.is_stopped()):
+		velocity.y -= jumpspeed
+		if velocity.y < -250:
+			velocity.y = -250
+
 		
-	if not is_on_floor() and not is_on_wall():
+	if !is_on_floor():
 		velocity.y += gravity
 		
-	if is_on_wall() and direction.x > 0 and Input.is_action_pressed("right") and velocity.y > 0:
-		velocity.y = gravity * wallslide
-	if is_on_wall() and direction.x < 0 and Input.is_action_pressed("left") and velocity.y > 0:
-		velocity.y = gravity * wallslide
+	if is_on_wall() and direction.x > 0 and Input.is_action_pressed("right") and !is_on_floor():
+		if !Input.is_action_pressed("jump"):
+			velocity.y = gravity * wallslide
+			velocity.x = direction.x * speed * 0.5
+		if Input.is_action_just_pressed("jump"):
+			$WalljumpTimer.start()
+			velocity.x = -direction.x * speed * 2
+			velocity.y = -200
+
+
+	if is_on_wall() and direction.x < 0 and Input.is_action_pressed("left") and !is_on_floor():
+		if !Input.is_action_pressed("jump"):
+			velocity.y = gravity * wallslide
+			velocity.x = direction.x * speed * 0.5
+		if Input.is_action_just_pressed("jump"):
+			$WalljumpTimer.start()
+			velocity.x = -direction.x * speed * 2
+			velocity.y = -200
 	
 	was_on_floor = is_on_floor()
+
 	
 	move_and_slide()
 	
-	if was_on_floor && !is_on_floor():
+	if was_on_floor and !is_on_floor():
 		$CoyoteTimer.start()
-	
-	if Input.is_action_pressed("jump") or velocity.y > 0:
+	elif Input.is_action_pressed("jump") or velocity.y > 0:
 		jump_anim(direction.x)
 	elif Input.is_action_pressed("run") and direction.length() > 0:
 		last_direction = direction.x
@@ -57,7 +125,13 @@ func _physics_process(_delta: float) -> void:
 		idle_anim(last_direction)
 
 
-
+func walljump_anim(direction):
+	if direction > 0:
+		$PlayerAnimation.flip_h = true
+		$PlayerAnimation.play("jump")
+	if direction < 0:
+		$PlayerAnimation.flip_h = false
+		$PlayerAnimation.play("jump")
 
 func jump_anim(direction):
 	if direction < 0:
